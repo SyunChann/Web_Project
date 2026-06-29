363421ca048df6d005719163c3256f2f31e8efb8
SyunChann <chltmdcks3@gmail.com>
2026-06-29 18:55:11 +0900

    Add: Vercel Speed Insights 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Analytics } from "@vercel/analytics/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
@@ -24,6 +25,7 @@ export default function RootLayout({
       <body>
         {children}
         <Analytics />
+        <SpeedInsights />
       </body>
     </html>
   );
